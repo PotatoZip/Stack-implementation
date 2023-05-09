33ca3e36bf5cd05d0d9e1d04cb790fa14434d6ad
WiktorZmiendak <124774374+PotatoZip@users.noreply.github.com>
2023-05-09 15:43:47 +0200

    Add files via upload

--- Stack_implementation/Stack_implementation/Menu.cpp
+++ Stack_implementation/Stack_implementation/Menu.cpp
@@ -5,12 +5,12 @@
 void PrintMenu() {
 	int ans;
 	printf("============================\nStack implementation program\n============================\n\n");
-	printf("Select option:\n1 -> Implement stack\n2 -> Delete stack\n3 -> Add new element to stack\n4 -> Show first element of stack\n5 -> Find element in stack\n6 -> Save all stack elements to binary file\n7 -> Open stack from file\n");
+	printf("Select option:\n1 -> Initialize stack\n2 -> Delete stack\n3 -> Add new element to stack\n4 -> Show first element of stack\n5 -> Find element in stack\n6 -> Save all stack elements to binary file\n7 -> Open stack from file\n");
 	scanf_s("%d", &ans);
 
 	switch (ans) {
 		case 1:
-			//ImplementStack();
+			
 			break;
 		case 2:
 			//ImplementStack();
